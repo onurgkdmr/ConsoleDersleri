9c1e745936c573ae16010497713f91dcf9d1d7b7
onurgkdmr <onurgkdmr.55@gmail.com>
2026-01-16 00:58:33 +0300

    Operatörler konusuna ait örnekler çözüldü ve ödev yapıldı.

--- 260115_4_Ornek_Operatorler/Program.cs
+++ 260115_4_Ornek_Operatorler/Program.cs
@@ -20,6 +20,7 @@
 			Console.WriteLine("14 >= 14 && (45 == 45 && \"software\"==\"yazılım\") sonuç:" + sonuc);
 
 
+			// SORU-1:
 			sonuc = 'a' != 'A' && (14 >= 4 || (DateTime.Now.Year == 2026 && true != false)); //İşlem sonucunu adım adım yazınız.
 			//---------True    && ( True   || (         True             &&      True    ))
 			//---------True    && ( True   || (                    True                  ))
@@ -29,6 +30,7 @@
 			Console.WriteLine("'a' != 'A' && (14 >= 4 || (DateTime.Now.Year == 2026 && true != false)) sonucu:" + sonuc);
 
 
+			// SORU-2:
 			sonuc = "Yaşar Kemal Van" != "Orhan Pamuk İstanbul" && ((14.5 == Convert.ToDouble("14.5") || false == true) && (double)45 == Convert.ToDouble("45")); // Adım adım çözümleri veriniz.
 			//------------------------True                      && ((           True                  ||     False    ) &&               True                  )
 			//------------------------True                      && ((                      True                       ) &&               True                  )
