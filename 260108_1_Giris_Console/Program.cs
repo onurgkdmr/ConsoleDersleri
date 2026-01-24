e1f74a9e0eb268cbb520c85d3d7942d73d3bfaaa
onurgkdmr <onurgkdmr.55@gmail.com>
2026-01-25 00:45:07 +0300

    Giriş

---
+++ 260108_1_Giris_Console/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _260108_1_Giris_Console
+{ // Başla-1 => İlk giren
+	internal class Program
+	{ // Başla-2
+		static void Main(string[] args)
+		{ // Başla-3
+
+
+
+			// Tek satırlık yorum satırı ''//'' ile gösterilir.
+
+			// Birden fazla satır için yorum satırı ''/* */'' ile gösterilir.
+			/*
+             Birden
+            fazla satır için
+            yorum satırı
+            */
+
+			// Yorum satırları proje içinde derlenmez (okunmaz). Kod açıklamaları yapmak için kullanılır.
+
+			// Ekrana yazı yazdırmak için aşağıdaki kod kullanılır.
+			Console.WriteLine("C# yazılım dersleri- Arı Bilgi Dershaneleri");
+			Console.WriteLine("Kartal Sahil Yolu");
+			Console.WriteLine("C# nesne tabanlı bir programlama dilidir.");
+
+			// Yukarıdaki kodların sonucunu Console ekranında görmek için projeyi çalıştırmak gerekir. Projeyi çalıştırmak için klavyeden Ctrl+F5 tuşlarını kullanabilirsiniz.
+			// Ctrl+F5 : Projeyi derler ve çalıştırır.
+			// Özel karakterler => *,-,+,/,%,?,',^,~,&,<,>,#,$,{,]...
+
+			Console.WriteLine("Onur Gökdemir - İnşaat Mühendisi");
+
+
+
+		} // Bitir-3
+	} // Bitir-2
+} // Bitir-1 => Son çıkar (Başla-1 için kapanan parantez).
