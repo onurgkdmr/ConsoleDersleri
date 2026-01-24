b7ae7ea3ceb5b7fbc18712b5213592831e41b95b
onurgkdmr <onurgkdmr.55@gmail.com>
2026-01-25 02:13:55 +0300

    Mantıksal Değişkenler (Veri tipi)

---
+++ 260108_4_Metinsel_Degiskenler/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _260108_4_Metinsel_Degiskenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CTRL+SPACE ile kodlar görünebilir.
+
+            string metin1 = "Merhaba dünya";  // RAM ne kadar güçlü ise o kadar metin (string) ifade alır.
+
+            string metin2 = "Metin, çift tırnak içinde tanımlanır.";
+
+            Console.WriteLine("Merhaba metin yazısı");
+            Console.WriteLine(metin1);
+
+
+            char karakter1 = 'A';  // Tek karakter tutan değişken türüdür.
+            char karakter2 = 'B';
+			char karakter3 = '?';
+
+
+		}
+	}
+}
