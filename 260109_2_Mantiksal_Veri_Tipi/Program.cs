11dd5d178b51128d870244dd5689fed3c93cebad
onurgkdmr <onurgkdmr.55@gmail.com>
2026-01-25 23:39:17 +0300

    Mantıksal Veri Tipi

---
+++ 260109_2_Mantiksal_Veri_Tipi/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _260109_2_Mantiksal_Veri_Tipi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Doğru => True, Yanlış => False
+
+            bool mantik1 = true;
+            bool mantik2 = false;
+
+            Console.WriteLine(mantik1);
+
+            // < : Küçüktür anlamına gelir.
+            // > : Büyüktür anlamına gelir.
+
+            bool durum1 = 10 > 9;  // True
+            bool durum2 = 10 < 9;  // False
+
+            Console.WriteLine("10>9 Sonuç:" + durum1);
+			Console.WriteLine("10<9 Sonuç:" + durum2);
+
+		}
+	}
+}
