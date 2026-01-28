7cdd6f02f61938ffecd1e4d507d4159a275e6eb3
onurgkdmr <onurgkdmr.55@gmail.com>
2026-01-29 01:18:48 +0300

    Parse Tip Dönüşümü (Güncellenecek).

---
+++ 260113_1_Parse_Tip_Donusumu/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _260113_1_Parse_Tip_Donusumu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
