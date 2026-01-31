cd34bfd856823e3a051524c6052a83553ce878ed
onurgkdmr <onurgkdmr.55@gmail.com>
2026-02-01 00:08:12 +0300

    Ödev

--- 260113_5_Odevler/Program.cs
+++ 260113_5_Odevler/Program.cs
@@ -13,6 +13,9 @@
             5) 1. , 2. ve 3. sayılarının çarpım sonucunun 4. sayıya bölünmesi
              */
 
+
+            // SORU-1:
+
             Console.WriteLine("1. sayıyı giriniz:");
             int sayi1 = Convert.ToInt32(Console.ReadLine());
 
@@ -28,14 +31,22 @@
             int toplam = sayi1 + sayi2 + sayi3 + sayi4;
             Console.WriteLine("4 sayının toplamı:" + toplam);
 
+            // SORU-2:
+
             double sonuc1 = (sayi1 * sayi2) / (double)sayi4;
             Console.WriteLine("1. ve 2. sayıların çarpım sonucunun 4. sayıya bölümü:" + sonuc1);
+
+            // SORU-3:
 
             double sonuc2 = (sayi1 + sayi2) / ((double)sayi3 - sayi4);
             Console.WriteLine("1. ve 2. sayıların toplam sonucunun, 3. sayıdan 4. sayının çıkartılması sonucuna bölümü:" + sonuc2);
 
+            // SORU-4:
+
             double sonuc3 = (sayi1 + sayi2 + sayi3) / (double)sayi4;
             Console.WriteLine("1. , 2. ve 3. sayıların toplamının 4. sayıya bölümü:" + sonuc3);
+
+            // SORU-5:
 
             double sonuc4 = (sayi1 * sayi2 * sayi3) / (double)sayi4;
             Console.WriteLine("1. , 2. ve 3. sayıların çarpım sonucunun 4. sayıya bölümü:" + sonuc4);
