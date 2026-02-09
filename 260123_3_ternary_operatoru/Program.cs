13abf5db7af77c074d130f1a9bec905c68c9f8b9
onurgkdmr <onurgkdmr.55@gmail.com>
2026-02-09 23:50:43 +0300

    Ternary öperatörü

---
+++ 260123_3_ternary_operatoru/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _260123_3_ternary_operatoru
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ternary operatörü => if else yerine kullanılan ?: operatörleridir.
+            // Kullanıcıdan alınan sayının pozitif mi negatif mi olduğunu ternary ile hesaplayalım.
+
+            BASLA:
+            Console.WriteLine("Sayı giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+            
+            string sonuc;
+            // sayi > 0 ? "Pozitif" : "Negatif" ;
+            //   Koşul  ?   True    :   False   ;
+
+            Console.WriteLine("--- if - else ile: ---");
+
+            if (sayi > 0)
+            {
+                Console.WriteLine("Pozitif:"+sayi);
+            }
+            else
+            {
+                Console.WriteLine("Negatif:"+sayi);
+			}
+
+            Console.WriteLine("--- ternary ile ---");
+
+            //Console.WriteLine(sayi > 0 ? "Pozitif:"+sayi : " Negatif:"+sayi);
+            sonuc = sayi > 0 ? "Pozitif" : "Negatif";
+            Console.WriteLine(sonuc + ":" + sayi);
+
+            Console.WriteLine("------------------------------------------");
+            goto BASLA;
+            
+		}
+    }
+}
