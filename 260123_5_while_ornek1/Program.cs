e0eba78c781409143d7b803161ad80ceeca840b5
onurgkdmr <onurgkdmr.55@gmail.com>
2026-02-10 02:36:47 +0300

    Örnek

---
+++ 260123_5_while_ornek1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _260123_5_while_ornek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 5-135 arasındaki sayıları ekrana yazdırınız.
+
+            int sayi = 5;
+
+            while (sayi <= 135)
+            {
+                Console.WriteLine(sayi);
+                sayi++;
+            }
+
+            Console.WriteLine("-----------------------------------");
+
+            // 5-135 arasındaki sayıların toplamını hesaplayan kod yapısını yazınız.
+
+            int sayi1 = 5;
+            int toplam = 0;
+
+            while (sayi1<=135)
+            {
+                toplam = toplam + sayi1;
+                sayi1++;
+            }
+            
+            Console.WriteLine("5-135 arasındaki sayıların toplamı:" + toplam);
+        }
+    }
+}
