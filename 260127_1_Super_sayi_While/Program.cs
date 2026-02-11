62df82dbaff8aced954cc331f676f53b9a614dd9
onurgkdmr <onurgkdmr.55@gmail.com>
2026-02-12 02:07:25 +0300

    Örnek

---
+++ 260127_1_Super_sayi_While/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _260127_1_Super_sayi_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bölünenlerinin toplamı kendisini veren sayılara süper sayı denir (kendisi dahil değil). 1-100000 arasındaki süper sayıları listeleyiniz.
+
+            int superSayi = 0;
+            
+            while (superSayi<=100000)
+            {
+				int bolenSayi = 1;
+				int toplam = 0;
+
+				while (superSayi > bolenSayi)
+				{
+					if (superSayi % bolenSayi == 0)
+					{
+						toplam = toplam + bolenSayi;
+					}
+					bolenSayi++;
+				}
+				if (toplam == superSayi)
+				{
+					Console.WriteLine("Bu bir süper sayıdır:" + superSayi);
+				}
+				superSayi++;
+			}
+            
+            
+		}
+    }
+}
