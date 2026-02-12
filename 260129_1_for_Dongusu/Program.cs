4aa8aef427b4fce41639cb64e4d112047e192a2a
onurgkdmr <onurgkdmr.55@gmail.com>
2026-02-12 17:11:44 +0300

    For Döngüsü

---
+++ 260129_1_for_Dongusu/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _260129_1_for_Dongusu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // for döngüsü:
+
+            // Genelde sayısal işlemlerde kullanılır ama her türlü de kullanılabilir. Tamamen kullanıma bağlıdır.
+
+            int sayac = 0; // while ve do while döngülerinde bu şekilde bir değişkene ihtiyaç duyulur ama for döngüsü bu yapıyı kendi içinde getirir.
+
+            while ( sayac<100)
+            {
+                Console.WriteLine(sayac);
+                sayac++;
+            }
+
+            // Yukarıdaki while ile aşağıdaki for'u karşılaştıralım.
+
+            // for (1.Adım, 2.Adım, 3.Adım)
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i+",");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("for ile tersten sayma işlemi:");
+
+            for (int i=25; i>10; i--)
+            {
+                Console.WriteLine(i + ",");
+            }
+        }
+    }
+}
