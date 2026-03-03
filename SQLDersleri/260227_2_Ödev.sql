bceed88181e88e056e0832bf2b368a386b8394fc
onurgkdmr <onurgkdmr.55@gmail.com>
2026-03-03 20:04:38 +0300

    Ödev

--- SQLDersleri/260227_2_Ödev.sql
+++ SQLDersleri/260227_2_Ödev.sql
@@ -11,26 +11,34 @@ select *from Products
 select *from [Order Details]
 select 
 (select p.ProductName from Products as p where p.ProductID=od.ProductID) as 'Ürün Adı',
-COUNT(Quantity) as 'Satılan Ürün Adedi',
+SUM(Quantity) as 'Satılan Ürün Adedi',
 (select p.UnitPrice from Products as p where p.ProductID=od.ProductID) as 'Birim Fiyatı',
 (select p.UnitsInStock from Products as p where p.ProductID=od.ProductID) as 'Kalan stok miktarı'
 from [Order Details] as od group by od.ProductID order by [Satılan Ürün Adedi]
 
 
 -- SORU-2:
-select *from [Order Details]
-select *from Orders
-select *from Products
+
 select 
-od.OrderID,
-COUNT(od.ProductID) as 'Ürün Sayısı' 
-from [Order Details] as od group by od.OrderID
+(select OrderDate from Orders as o where o.OrderID=od.OrderID) as 'Tarih',
+OrderID, SUM(Quantity) as 'Ürün Sayısı'
+from [Order Details] as od
+group by OrderID
 
 -- SORU-3:
 select *from Customers
-select 
+select *from Orders
+select *from [Order Details]
+
+select
+distinct
 (select c.CompanyName from Customers as c where c.CustomerID=o.CustomerID) as 'Firma Adı',
 (select c.ContactName from Customers as c where c.CustomerID=o.CustomerID) as 'Firma Yetkilisi Ad-Soyadı',
 (select c.City from Customers as c where c.CustomerID=o.CustomerID) as 'Şehir',
-(select c.Country from Customers as c where c.CustomerID=o.CustomerID) as 'Ülke'
-from Orders as o group by o.CustomerID
+(select c.Country from Customers as c where c.CustomerID=o.CustomerID) as 'Ülke',
+o.OrderID,
+COUNT(o.OrderID) as 'Sipariş Sayısı',
+(select SUM(Quantity) from [Order Details] as od where od.OrderID=o.OrderID)
+from Orders as o group by o.CustomerID,o.OrderID
+
+-- ONUR
