9811240d6aa6bf038b7f278c73a4b591c023597c
onurgkdmr <onurgkdmr.55@gmail.com>
2026-03-27 21:36:37 +0300

    SP ve SP ile Crud İşlemleri

---
+++ SQLDersleri/260326_2_Stored Procedure.sql
@@ -0,0 +1,53 @@
+-- ****************************************
+-- 26 Mart 2026 SQL Dersleri
+-- ****************************************
+-- Stored Procedure
+-- Kısaca SP olarak isimlendirilir. SP'ler performans konusunda çok iyi oldukları için çok kullanılan bir nesnedir.
+-- Saklı yordamlar olarak bilinir.
+/*
+SP lerin özellikleri:
+1-Performansları çok başarıldır. 
+2-CRUD işlemleri SP ler ile yapılır
+3-Parametre alır, döngü, karar yapıları , case when then yapıları kullanılabilir
+*/
+
+use Nortwinddb
+
+-- SP Oluşturulması:
+
+go
+create procedure sp_Kategori_Urunler
+as
+begin
+select
+c.CategoryName,p.ProductName,p.UnitPrice,p.UnitsInStock
+from Categories c
+join Products as p on p.CategoryID=c.CategoryID
+end
+go
+
+-- SP Çağrılması:
+
+execute sp_Kategori_Urunler
+exec sp_Kategori_Urunler
+
+----------------------------------------------------------------
+
+go
+create proc sp_Kategori_Urun_Ara(@aranacakKelime nvarchar(100))
+as
+begin
+select
+c.CategoryName,p.ProductName,p.UnitPrice,p.UnitsInStock
+from Categories c
+join Products as p on p.CategoryID=c.CategoryID
+where p.ProductName like '%'+@aranacakKelime+'%'
+end
+go
+
+execute sp_Kategori_Urun_Ara 'c'
+exec sp_Kategori_Urun_Ara 'ch'
+
+
+
+-- ONUR
---
+++ SQLDersleri/260327_2_SP_CRUD.sql
@@ -0,0 +1,128 @@
+-- **********************************************
+-- 27 Mart 2026 SQL Dersleri
+-- **********************************************
+-- SP ile CRUD İşlemleri
+-- SP ile Insert, Update, Delete işlemlerinin yapılması
+
+use HastahaneDB
+
+select * from Doktorlar
+select * from Branslar
+
+
+insert into Branslar(Adi,Aciklama) values ('Göz','Uzmanlık alanda hizmet verir.')
+
+go
+create proc sp_Brans_Ekle
+(
+@adi nvarchar(250),
+@aciklama nvarchar(MAX)
+)
+as
+begin
+insert into Branslar(Adi,Aciklama) values (@adi,@aciklama)
+end
+go
+
+execute dbo.sp_Brans_Ekle 'Göz','Test'
+execute dbo.sp_Brans_Ekle 'Göz','Test-1'
+
+select *from Branslar
+
+-- Aynı datanın insert edilmesini engellemek için aşağıdaki işlemi SP'ye alter yaparak uygulayalım.
+
+go
+alter proc sp_Brans_Ekle
+(
+@adi nvarchar(250),
+@aciklama nvarchar(MAX)
+)
+as
+begin
+
+ declare @dataSayisi int = (select COUNT(Id) from Branslar where Adi=@adi)
+if(@dataSayisi>1)
+    begin--{
+     print 'Bu data DB de mevcut'
+    end--}
+else
+    begin--{
+    insert into Branslar(Adi,Aciklama) values (@adi,@aciklama)
+    end--}
+end
+go
+
+execute dbo.sp_Brans_Ekle 'Göz','Test'
+
+------------------------------------------------------------------------
+
+go
+create proc sp_Brans_Update
+(
+@id int,
+@adi nvarchar(250),
+@aciklama nvarchar(MAX)
+)
+as
+begin
+ declare @dataSayisi int = (select COUNT(Id) from Branslar where Adi=@adi)
+if(@dataSayisi>0)
+    begin--{
+     print 'Bu data DB de mevcut'
+    end--}
+else
+    begin--{
+    update Branslar set Adi=@adi,Aciklama=@aciklama where Id=@id
+    end--}
+end
+go
+
+select *from Branslar
+
+exec sp_Brans_Update 4,'Fizik Tedavi','Teknisyen fizyoterapist bakmaktadır.'
+exec sp_Brans_Update 5,'Fizik Tedavi','Teknisyen fizyoterapist bakmaktadır.'
+
+-----------------------------------------------------------------------------------
+
+go
+alter proc sp_Brans_Update
+(
+@id int,
+@adi nvarchar(250),
+@aciklama nvarchar(MAX)
+)
+as
+begin
+-- declare @dataSayisi int = (select COUNT(Id) from Branslar where Adi=@adi)
+-- Exists => Data varsa true, yoksa false verir.
+if(Exists(select * from Branslar where Adi=@adi))
+    begin--{
+     print 'Bu data DB de mevcut'
+    end--}
+else
+    begin--{
+    -- İç içe if-else blokları SQL içinde kullanılır.
+    if(Exists(select * from Branslar where Id=@id))
+    begin
+    update Branslar set Adi=@adi,Aciklama=@aciklama where Id=@id
+    end--}
+    else
+    begin
+    print 'Bu Id değerinde bir Brans yok'
+    end
+    end
+end
+go
+
+exec sp_Brans_Update 69,'Fizik Tedavi-5','Teknisyen fizyoterapist bakmaktadır.'
+
+-- ÖDEV: 
+-- CRUD konusunda yapılan ödevler için SP yapılacak.
+/*
+-- 30 tane Randevular, diğer tablolar için 10'ar tane data SP yapılarak eklenecek.
+Koşul 1 => Her tablo için SP Update işlemi de yapılacak.
+Koşul 2 => Tekrarlı data eklemesi engellenecek (Insert-Update için ayrı ayrı yapılacak).
+*/
+
+
+-- ONUR
